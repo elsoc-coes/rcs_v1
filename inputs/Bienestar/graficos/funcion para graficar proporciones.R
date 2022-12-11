1fc09febb6bcadda8c1a870923f912e10794cdb9
CarlosDelgadoG <carlos.delgado@ug.uchile.cl>
2022-12-11 12:10:07 -0300

    avances graficos
    desigualdad de ingreso, justicia distrbutiva, identidad nacional, meritocracia

---
+++ inputs/Bienestar/graficos/funcion para graficar proporciones.R
@@ -0,0 +1,45 @@
+
+
+grafo_prop_var <- function(var_x,var_y,umbral=c(4,5),titulo,limy_low=0,
+                           limy_sup=1,guardar=FALSE,guardar_como="grafico",imprimir=TRUE){
+  grafico<-  elsoc_bienestar%>%
+    filter(tipo_atricion==1)%>%
+    sjlabelled::as_label(ola) %>%
+    mutate(esfuerzo=as.numeric(!!rlang::sym(var_x) %in% umbral))%>%
+    prop_list(esfuerzo,by=c(ola,!!rlang::sym(var_y)),na.rm=TRUE)%>%
+    filter(value==1)%>%
+    drop_na()%>%
+    ggplot(aes(x=factor(ola),y=prop,color=!!rlang::sym(var_y),group=!!rlang::sym(var_y),
+               label = scales::percent(prop, accuracy = .1)))+
+    geom_point()+
+    geom_line()+
+    geom_text_repel(nudge_y = .01, size = 3, color = 'black') +
+    theme_bw()+
+    ylab(label = NULL) +
+    xlab(label = NULL) +
+    scale_color_viridis_d(begin = 0, end = .85, option = 'viridis')+
+    scale_y_continuous(labels = scales::percent, limits = c(limy_low,limy_sup))+
+    theme(plot.caption = element_text(hjust = 0),
+          legend.position = 'top',
+          legend.title = element_blank())+
+    ggtitle(titulo,
+            subtitle = gsub(x=attr(getElement(elsoc_bienestar,var_x),"label"),
+                            pattern = "Grado de acuerdo: ",
+                            replacement = ""))+
+    labs(caption = 'Fuente: Elaboración propia en base a datos ELSOC 2016-2022.')
+  
+  
+  if(guardar){
+    
+    saveRDS(grafico,
+            file=paste0("inputs/Bienestar/graficos/",
+                        guardar_como,
+                        ".RDS"))
+  }
+  
+  if(imprimir){
+    return(grafico)  
+  }
+  
+  
+}   
